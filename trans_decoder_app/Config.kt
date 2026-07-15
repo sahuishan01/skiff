25357e851140a7ae2cc6dbd9bef54151e77a9a1d
Skiff Developer <developer@skiff.p2p>
2026-07-15 23:42:22 +0530

    Update client config with production domain and configure GitHub release workflow for v0.0.1 APK

---
+++ trans_decoder_app/Config.kt
@@ -0,0 +1,6 @@
+package com.transdecoder
+
+object Config {
+    const val SIGNALING_SERVER_URL = "wss://skiff.algosculptor.com/ws"
+    const val API_SERVER_URL = "https://skiff.algosculptor.com"
+}
