4cf190bf817e8013e41c06548811c32bde76d3e1
sahuishan01 <ishan20015.s@gmail.com>
2026-08-24 12:25:20 +0530

    chore: bump version to v0.0.30

--- trans_decoder_app/app/build.gradle.kts
+++ trans_decoder_app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.transdecoder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 29
-        versionName = "0.0.29"
+        versionCode = 30
+        versionName = "0.0.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
